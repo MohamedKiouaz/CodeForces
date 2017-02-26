5ceeeb021105630b6cf08f9a60866e35d43b58b5
indetronable <mohamed.kiouaz@gmail.com>
2017-02-26 09:18:17 +0100

    Easy

---
+++ cpp/513A.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    unsigned n1, n2;
+	std::cin >> n1 >> n2;
+    if(a > b)
+        std::cout << "First";
+    else
+        std::cout << "Second";
+    return 0
+}
