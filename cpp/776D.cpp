ff6128797683aaa6f28bf6bfb9eda06946da87f1
indetronable <mohamed.kiouaz@gmail.com>
2017-02-26 09:18:48 +0100

    Too hard ! :(

---
+++ cpp/776D.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    unsigned n, m, x;
+	std::cin >> n >> m;
+    bool room[10000] = {false};
+    for(unsigned i = 0; i < n; i++)
+        std::cin >> room[i];
+    for(unsigned i = 0; i < m; i++) {
+        std::cin >> x;
+        for(unsigned j = 0; j < x; j++) {
+            
+        }
+    }
+}
